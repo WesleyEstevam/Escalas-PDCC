69a0480391a780e8c73c3bfbcb14c871c0a613eb
WesleyEstevam <wesleyestevam15@gmail.com>
2024-07-02 11:24:03 -0300

    cron job implementado

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cron from 'node-cron';
+import axios from 'axios';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,5 +9,16 @@ async function bootstrap() {
   await app.listen(3001, () => {
     console.log('Server is running');
   });
+
+  // Configurar o cron job para enviar requisições a cada 5 minutos
+  cron.schedule('*/5 * * * *', async () => {
+    try {
+      await axios.get('http://localhost:3001/coroinhas'); // ou URL pública da sua aplicação
+      console.log('Ping successful');
+    } catch (error) {
+      console.error('Ping failed:', error.message);
+    }
+  });
 }
+
 bootstrap();
